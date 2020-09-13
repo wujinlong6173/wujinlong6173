ba0aa926aae4879779ba11888ce86964bc11f64b
wujinlong <wujinlong6173@outlook.com>
2020-09-13 17:30:00 +0800

    虚拟路由器支持配置静态路由

--- src/main/java/com/huawei/cli/VirRouterView.java
+++ src/main/java/com/huawei/cli/VirRouterView.java
@@ -1,6 +1,9 @@
 package com.huawei.cli;
 
+import com.huawei.common.CLI;
 import com.huawei.common.Interface;
+import com.huawei.inventory.PhyRouter;
+import com.huawei.inventory.PhyRouterMgr;
 import com.huawei.vrf.Vrf;
 import com.huawei.vrf.VrfMgr;
 import wjl.cli.Command;
@@ -9,6 +12,7 @@ import wjl.cli.CommandView;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * 配置虚拟路由器
@@ -43,17 +47,35 @@ public class VirRouterView implements CommandView {
     }
 
     @Command(command="interface {name}")
-    public Object cfgInterface(String name) {
+    public Object cfgInterface(String portName) {
         Vrf vrf = VrfMgr.getVrf(id);
         if (vrf == null) {
             return "Error : virtual router is deleted.";
         }
 
-        Interface inf = vrf.getBindInterfaces().get(name);
+        Interface inf = vrf.getBindInterfaces().get(portName);
         if (inf == null) {
             return "Error : please create interface before config.";
         }
 
-        return new VirInterfaceView(inf.getId(), name);
+        return new VirInterfaceView(inf.getId(), portName);
+    }
+
+    @Command(command="ip static-route {dst} out {port} next-hop {nextIp}")
+    public String addStaticRoute(String dst, String port, String nextIp) {
+        Vrf vrf = VrfMgr.getVrf(id);
+        if (vrf == null) {
+            return "Error : virtual router is deleted.";
+        }
+
+        Interface inf = vrf.getBindInterfaces().get(port);
+        if (inf == null) {
+            return String.format(Locale.ENGLISH, "Error : port %s does not exist.", port);
+        }
+
+        PhyRouter pr = PhyRouterMgr.getRouter(vrf.getHost());
+        pr.addConfig(CLI.IP, CLI.STATIC_ROUTE, CLI.VPN_INSTANCE, vrf.getName(), dst,
+                CLI.OUT, inf.getInterfaceName(), CLI.NEXT_HOP, nextIp);
+        return null;
     }
 }
--- src/main/java/com/huawei/common/CLI.java
+++ src/main/java/com/huawei/common/CLI.java
@@ -6,5 +6,8 @@ public class CLI {
     public static final String BINDING = "binding";
     public static final String INTERFACE = "interface";
     public static final String IP = "ip";
+    public static final String NEXT_HOP = "next-hop";
+    public static final String OUT = "out";
+    public static final String STATIC_ROUTE = "static-route";
     public static final String VPN_INSTANCE = "vpn-instance";
 }
