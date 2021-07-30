a55b0fb39a85ce18beea1b27becf19c17efd4f2d
wujinlong <wujinlong6173@outlook.com>
2021-07-30 22:02:20 +0800

    add SimplePath.contain

--- wjl-mapping/src/main/java/wjl/mapping/core/model/SimplePath.java
+++ wjl-mapping/src/main/java/wjl/mapping/core/model/SimplePath.java
@@ -1,5 +1,7 @@
 package wjl.mapping.core.model;
 
+import java.util.Objects;
+
 /**
  * 属性名和整数下标构成的路径，表示 abc.items[2].name 这样的路径。
  */
@@ -16,32 +18,27 @@ public class SimplePath {
         this.nodes = nodes;
     }
 
-    public String toString(boolean withBrace) {
-        StringBuilder sb = new StringBuilder();
-        if (withBrace) {
-            sb.append("${");
-        }
-        boolean requireDot = false;
-        for (Object node : nodes) {
-            if (node instanceof Integer) {
-                sb.append("[").append(node).append("]");
-            } else if (node instanceof String) {
-                String str = (String)node;
-                if (str.contains(".")) {
-                    sb.append("[\"").append(node).append("\"]");
-                } else {
-                    if (requireDot) {
-                        sb.append(".");
-                    }
-                    sb.append(node);
+    /**
+     * 是否存在包含关系。
+     *
+     * @param sub 被包含的路径
+     * @return 相等或包含时返回真
+     */
+    public boolean contain(SimplePath sub) {
+        if (sub == null) {
+            return false; // 容错
+        } else if (this == sub) {
+            return true; // 相同
+        } else if (this.nodes.length > sub.nodes.length) {
+            return false;
+        } else {
+            for (int idx = 0; idx < this.nodes.length; idx++) {
+                if (!Objects.equals(this.nodes[idx], sub.nodes[idx])) {
+                    return false;
                 }
             }
-            requireDot = true;
+            return true; // 相等或包含
         }
-        if (withBrace) {
-            sb.append("}");
-        }
-        return sb.toString();
     }
 
     /**
@@ -72,4 +69,33 @@ public class SimplePath {
     public String toString() {
         return toString(true);
     }
+
+    public String toString(boolean withBrace) {
+        StringBuilder sb = new StringBuilder();
+        if (withBrace) {
+            sb.append("${");
+        }
+        boolean requireDot = false;
+        for (Object node : nodes) {
+            if (node instanceof Integer) {
+                sb.append("[").append(node).append("]");
+            } else if (node instanceof String) {
+                String str = (String)node;
+                if (str.contains(".")) {
+                    sb.append("[\"").append(node).append("\"]");
+                } else {
+                    if (requireDot) {
+                        sb.append(".");
+                    }
+                    sb.append(node);
+                }
+            }
+            requireDot = true;
+        }
+        if (withBrace) {
+            sb.append("}");
+        }
+        return sb.toString();
+    }
+
 }
--- wjl-mapping/src/test/java/wjl/mapping/core/SimplePathTest.java
+++ wjl-mapping/src/test/java/wjl/mapping/core/SimplePathTest.java
@@ -18,4 +18,21 @@ public class SimplePathTest {
         Assert.assertEquals("${[\"ip.v6\"].prefix}",
                 new SimplePath("ip.v6", "prefix").toString());
     }
+
+    @Test
+    public void testContain() {
+        SimplePath p1 = new SimplePath("a", 0, "c");
+        SimplePath p2 = new SimplePath("a", 0, "c");
+        SimplePath p3 = new SimplePath("a", 1, "c");
+        SimplePath p4 = new SimplePath("a", 1);
+
+        Assert.assertTrue(p1.contain(p1));
+        Assert.assertTrue(p1.contain(p2));
+        Assert.assertTrue(p4.contain(p3));
+        Assert.assertTrue(SimplePath.EMPTY.contain(p3));
+        Assert.assertFalse(p1.contain(null));
+        Assert.assertFalse(p3.contain(p2));
+        Assert.assertFalse(p3.contain(p4));
+        Assert.assertFalse(p3.contain(SimplePath.EMPTY));
+    }
 }
